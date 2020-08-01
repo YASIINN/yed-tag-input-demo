96e9d939fbaceec0e545058cd3f668ce5d78589c
ydalkilic <ysndlklc1234@gmail.com>
2020-08-01 19:26:36 +0300

    added demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+import YedTagInput from 'yed-tag-input'
+import 'yed-tag-input/dist/index.css'
+
+const App = () => {
+  const initTags = []
+  const [tags, setTags] = useState(initTags)
+  const setTag = (tag) => {
+    setTags([...tags, tag])
+  }
+  const deleteTag = (item) => {
+    const deletedTags = JSON.parse(JSON.stringify(tags))
+    deletedTags.splice(item.index, 1)
+    setTags(deletedTags)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <>
+        <p>Hata Durumları İçerisinde Aynı Metinsel İfadeyi Barındırıyorsa Devreye Girecektir..</p>
+        <div className={'mt20'}>
+          <YedTagInput tags={tags} tagInputErrorControl={true} tagInpuTitle={'Kişileri Ekle'} onChange={setTag} removeTag={(item) => {deleteTag(item)}}/>
+        </div>
+        <div className="mt20">
+          <YedTagInput tags={tags} tagInputErrorControl={false} tagInpuTitle={'Kontrol Devre Dışı'} tagColor={'colorPurple'}
+                       onChange={setTag} removeTag={(item) => {
+            deleteTag(item)
+          }}/>
+        </div>
+        <div className="mt20">
+          <YedTagInput tags={tags} errorText={"Bu Kullanıcı Listeye Daha Önce Eklendi."} tagInputErrorControl={true} tagInpuTitle={'Özel Kontrol Mesajı İle Beraber Kullanım'} tagColor={'colorPink'}
+                       onChange={setTag} removeTag={(item) => {
+            deleteTag(item)
+          }}/>
+        </div>
+        <div className="mt20">
+          <YedTagInput tags={tags}  tagInputErrorColor={"blue"} tagInputTitleColor={"pink"}  tagInputErrorControl={true} tagInpuTitle={'Başlık Rengi ve Hata Rengi Özelleştirerek Kullanımı'} tagColor={"colorRed"}
+                       onChange={setTag} removeTag={(item) => {
+            deleteTag(item)
+          }}/>
+        </div>
+
+      </>
+  )
 }
 
-export default App;
+export default App
