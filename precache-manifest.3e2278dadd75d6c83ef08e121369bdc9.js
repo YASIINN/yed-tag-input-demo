db1cc7195c1c38c5a57d9b7dd9413498beaf8377
ydalkilic <ysndlklc1234@gmail.com>
2020-08-01 19:27:02 +0300

    Updates

---
+++ precache-manifest.3e2278dadd75d6c83ef08e121369bdc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "602e27f29710b0a5a277f024f74a92ea",
+    "url": "/yed-tag-input-demo/index.html"
+  },
+  {
+    "revision": "d209ce6c1c26bcc30857",
+    "url": "/yed-tag-input-demo/static/css/2.52bfa62f.chunk.css"
+  },
+  {
+    "revision": "5f92528391fb95de91a8",
+    "url": "/yed-tag-input-demo/static/css/main.02f9c359.chunk.css"
+  },
+  {
+    "revision": "d209ce6c1c26bcc30857",
+    "url": "/yed-tag-input-demo/static/js/2.2239066b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yed-tag-input-demo/static/js/2.2239066b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f92528391fb95de91a8",
+    "url": "/yed-tag-input-demo/static/js/main.dee95d91.chunk.js"
+  },
+  {
+    "revision": "917e079f9a7f4eaf0be2",
+    "url": "/yed-tag-input-demo/static/js/runtime-main.f6a1e1e9.js"
+  }
+]);
